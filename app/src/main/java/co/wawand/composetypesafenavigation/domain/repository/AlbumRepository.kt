00bd0f1092ba536c38c0098b02c17f1d6c94d0c0
Joe Arias C. <jarias@wawand.co>
2024-11-14 11:39:05 -0500

    adding domain model to load the albums with the photo list, adding repository function to retrieve the album with the photos

--- app/src/main/java/co/wawand/composetypesafenavigation/domain/repository/AlbumRepository.kt
+++ app/src/main/java/co/wawand/composetypesafenavigation/domain/repository/AlbumRepository.kt
@@ -2,6 +2,7 @@ package co.wawand.composetypesafenavigation.domain.repository
 
 import co.wawand.composetypesafenavigation.core.util.Resource
 import co.wawand.composetypesafenavigation.domain.model.Album
+import co.wawand.composetypesafenavigation.domain.model.AlbumWithPhotos
 import kotlinx.coroutines.flow.Flow
 
 interface AlbumRepository {
@@ -13,4 +14,6 @@ interface AlbumRepository {
     suspend fun retrieveLocalAlbums(): Flow<Resource<List<Album>>>
 
     suspend fun getAlbumDetails(id: Long): Flow<Resource<Album>>
+
+    suspend fun getAlbumDetailsWithPhotos(id: Long): Flow<Resource<AlbumWithPhotos>>
 }

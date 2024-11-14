63709fda7669456535b3d798227e0b56be92e0a0
Joe Arias C. <jarias@wawand.co>
2024-11-14 11:49:00 -0500

    adding modifier extension function for photo Grid Drag Handler

---
+++ app/src/main/java/co/wawand/composetypesafenavigation/presentation/extensions/ModifierExtentions.kt
@@ -0,0 +1,65 @@
+package co.wawand.composetypesafenavigation.presentation.extensions
+
+import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
+import androidx.compose.foundation.lazy.grid.LazyGridState
+import androidx.compose.runtime.MutableState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.hapticfeedback.HapticFeedback
+import androidx.compose.ui.hapticfeedback.HapticFeedbackType
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.unit.round
+import androidx.compose.ui.unit.toIntRect
+
+fun Modifier.photoGridDragHandler(
+    lazyGridState: LazyGridState,
+    haptics: HapticFeedback,
+    selectedIds: MutableState<Set<Long>>,
+    autoScrollSpeed: MutableState<Float>,
+    autoScrollThreshold: Float
+) = pointerInput(Unit) {
+    fun LazyGridState.gridItemKeyAtPosition(hitPoint: Offset): Long? =
+        layoutInfo.visibleItemsInfo.find { itemInfo ->
+            itemInfo.size.toIntRect().contains(hitPoint.round() - itemInfo.offset)
+        }?.key as? Long
+
+    var initialKey: Long? = null
+    var currentKey: Long? = null
+    detectDragGesturesAfterLongPress(
+        onDragStart = { offset ->
+            lazyGridState.gridItemKeyAtPosition(offset)?.let { key ->
+                if (!selectedIds.value.contains(key)) {
+                    haptics.performHapticFeedback(HapticFeedbackType.LongPress)
+                    initialKey = key
+                    currentKey = key
+                    selectedIds.value += key
+                }
+            }
+        },
+        onDragCancel = { initialKey = null; autoScrollSpeed.value = 0f },
+        onDragEnd = { initialKey = null; autoScrollSpeed.value = 0f },
+        onDrag = { change, _ ->
+            if (initialKey != null) {
+                val distFromBottom =
+                    lazyGridState.layoutInfo.viewportSize.height - change.position.y
+                val distFromTop = change.position.y
+                autoScrollSpeed.value = when {
+                    distFromBottom < autoScrollThreshold -> autoScrollThreshold - distFromBottom
+                    distFromTop < autoScrollThreshold -> -(autoScrollThreshold - distFromTop)
+                    else -> 0f
+                }
+
+                lazyGridState.gridItemKeyAtPosition(change.position)?.let { key ->
+                    if (currentKey != key) {
+                        selectedIds.value = selectedIds.value
+                            .minus(initialKey!!..currentKey!!)
+                            .minus(currentKey!!..initialKey!!)
+                            .plus(initialKey!!..key)
+                            .plus(key..initialKey!!)
+                        currentKey = key
+                    }
+                }
+            }
+        }
+    )
+}

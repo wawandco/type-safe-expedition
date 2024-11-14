54abcc189d39719a8e25b1249a56a0d82bc27e12
Joe Arias C. <jarias@wawand.co>
2024-11-14 11:51:58 -0500

    adjusting get albums use case to retrieve the photos

--- app/src/main/java/co/wawand/composetypesafenavigation/domain/usecase/GetAlbumsUseCase.kt
+++ app/src/main/java/co/wawand/composetypesafenavigation/domain/usecase/GetAlbumsUseCase.kt
@@ -1,6 +1,7 @@
 package co.wawand.composetypesafenavigation.domain.usecase
 
 import android.util.Log
+import co.wawand.composetypesafenavigation.core.Constant.GENERIC_ERROR
 import co.wawand.composetypesafenavigation.core.util.Resource
 import co.wawand.composetypesafenavigation.domain.model.Album
 import co.wawand.composetypesafenavigation.domain.repository.AlbumRepository
@@ -17,17 +18,73 @@ class GetAlbumsUseCase @Inject constructor(
         runCatching {
             albumRepository.retrieveRemoteAlbums()
         }.onFailure {
+            Log.d("GetAlbumsUseCase", "retrieveRemoteAlbums error: ${it.message}")
             it.printStackTrace()
-            emit(Resource.Error(it.message.toString()))
+            emit(Resource.Error(it.message ?: GENERIC_ERROR))
         }.onSuccess {
-            it.collect { result ->
-                if (result is Resource.Success) {
-                    albumRepository.retrieveLocalAlbums().collect { result ->
-                        emit(Resource.Success(result.data))
+            it.collect { retrieveRemoteAlbumsResult ->
+                when (retrieveRemoteAlbumsResult) {
+                    is Resource.Loading -> emit(Resource.Loading())
+                    is Resource.Error -> emit(
+                        Resource.Error(
+                            retrieveRemoteAlbumsResult.message ?: GENERIC_ERROR
+                        )
+                    )
+
+                    is Resource.Success -> {
+                        if (retrieveRemoteAlbumsResult.data == true) {
+                            runCatching {
+                                albumRepository.retrieveRemotePhotos()
+                            }.onFailure { retrieveRemotePhotosOnFailure ->
+                                Log.d(
+                                    "GetAlbumsUseCase",
+                                    "retrieveRemotePhotos error: ${retrieveRemotePhotosOnFailure.message}"
+                                )
+                                retrieveRemotePhotosOnFailure.printStackTrace()
+                                emit(
+                                    Resource.Error(
+                                        retrieveRemotePhotosOnFailure.message ?: GENERIC_ERROR
+                                    )
+                                )
+                            }.onSuccess { retrieveRemotePhotosOnSuccess ->
+                                retrieveRemotePhotosOnSuccess.collect { retrieveRemotePhotosResult ->
+                                    when (retrieveRemotePhotosResult) {
+                                        is Resource.Loading -> emit(Resource.Loading())
+                                        is Resource.Error -> emit(
+                                            Resource.Error(
+                                                retrieveRemotePhotosResult.message ?: GENERIC_ERROR
+                                            )
+                                        )
+
+                                        is Resource.Success -> {
+                                            if (retrieveRemotePhotosResult.data == true) {
+                                                albumRepository.retrieveLocalAlbums()
+                                                    .collect { localAlbumsResult ->
+                                                        when (localAlbumsResult) {
+                                                            is Resource.Loading -> emit(Resource.Loading())
+                                                            is Resource.Error -> emit(
+                                                                Resource.Error(
+                                                                    localAlbumsResult.message
+                                                                        ?: GENERIC_ERROR
+                                                                )
+                                                            )
+
+                                                            is Resource.Success -> emit(
+                                                                Resource.Success(
+                                                                    localAlbumsResult.data
+                                                                )
+                                                            )
+                                                        }
+                                                    }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        } else {
+                            emit(Resource.Success(emptyList()))
+                        }
                     }
-                } else {
-                    emit(Resource.Success(emptyList()))
-                    Log.d("GetAlbumsUseCase", "fail: ${result.data}")
                 }
             }
         }

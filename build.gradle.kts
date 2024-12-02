aed6965ef277d73f8266a9ca442dfb7eb8543637
Joe Arias C <jarias@wawand.co>
2024-12-02 17:14:38 -0500

    removing kapt plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,6 @@ plugins {
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.parcelize) apply false
-    alias(libs.plugins.kotlin.kapt) apply false
     alias(libs.plugins.google.services) apply false
 }
 
